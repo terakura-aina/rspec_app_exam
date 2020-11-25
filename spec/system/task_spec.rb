c57cf0bd29fc984995f0c91aa755287051147ad5
terakura aina <aicaierwei@gmail.com>
2020-11-25 14:25:18 +0900

    Taskが削除されること factoriesのtask.rbを修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -100,13 +100,13 @@ RSpec.describe 'Task', type: :system do
   describe 'Task削除' do
     context '正常系' do
       # FIXME: テストが失敗するので修正してください
-      xit 'Taskが削除されること' do
+      fit 'Taskが削除されること' do
         project = FactoryBot.create(:project)
         task = FactoryBot.create(:task, project_id: project.id)
         visit project_tasks_path(project)
         click_link 'Destroy'
         page.driver.browser.switch_to.alert.accept
-        expect(page).not_to have_content task.title
+        expect(page).not_to have_content(task.title)
         expect(Task.count).to eq 0
         expect(current_path).to eq project_tasks_path(project)
       end
